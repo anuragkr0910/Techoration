3cda5e41dd448fd70e978cd18d6d8eb2b79bb05c
Anurag Kumar <anuragkr0910@gmail.com>
2024-09-07 01:27:38 +0530

    DBContext Class created

---
+++ API/Techoration.API/Techoration.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Techoration.API.Models.Domain;
+
+namespace Techoration.API.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
