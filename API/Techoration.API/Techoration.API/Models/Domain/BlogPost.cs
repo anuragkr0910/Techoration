97ce73c92e8e86b9bc37f7690d1b1ba763b3647a
Anurag Kumar <anuragkr0910@gmail.com>
2024-09-07 01:01:54 +0530

    Domain Models Created

---
+++ API/Techoration.API/Techoration.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿namespace Techoration.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; } 
+
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Content { get; set; }
+
+        public int FeaturedImageURL { get; set; }
+
+        public int URLHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
---
+++ API/Techoration.API/Techoration.API/Models/Domain/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Techoration.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string URLHandle { get; set; }
+    }
+}
