0a77f8db4184b10003501372cc1d69e9c6343962
Anurag Kumar <anuragkr0910@gmail.com>
2024-09-07 03:52:09 +0530

    ConnectionString created, DbContext Injected

--- API/Techoration.API/Techoration.API/Program.cs
+++ API/Techoration.API/Techoration.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Techoration.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject DbContext
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TechorationConnectionString"));
+});
 
 var app = builder.Build();
 
